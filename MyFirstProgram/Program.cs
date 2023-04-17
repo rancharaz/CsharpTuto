2dd1b425364305af2522198765916adfb2e12f48
rancharaz <ranveer1990@hotmail.com>
2023-04-17 22:15:43 +0400

    first commit

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /** Console.WriteLine("I like pizza!");
+            Console.WriteLine("It's really good!");
+            Console.WriteLine("Test");**/
+            //This is a comment
+
+            /***
+             multiline comment
+             */
+
+           /** Console.WriteLine("\tRanveerAncharaz"); add a tab space
+            Console.WriteLine("\tRanveer\bAncharaz"); add a backspace
+            Console.WriteLine("\tRanveer\b \nAncharaz"); breakline
+
+            Console.ReadKey(); remove excess in console
+           
+            **/
+
+        }
+    }
+}
